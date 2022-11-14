729fb0aab7a003918b3e1327f74141a78f208774
zahir <zahiropel@gmail.com>
2022-11-14 21:51:51 +0600

    Display bord data to index.html item class

--- script/script.js
+++ script/script.js
@@ -4,4 +4,18 @@ const gameBord = function () {
     ["x", "o", "x"],
     ["o", "o", " x"],
   ];
+  return { bord };
 };
+const renderBord = function () {
+  let bord = gameBord().bord;
+  let selectBordEl = document.querySelectorAll(".item");
+  let selectBordArr = [...selectBordEl];
+  let bordCounter = 0;
+  for (let i = 0; i < bord.length; i++) {
+    for (let j = 0; j < 3; j++) {
+      selectBordArr[bordCounter].textContent = bord[i][j];
+      bordCounter++;
+    }
+  }
+};
+renderBord();
