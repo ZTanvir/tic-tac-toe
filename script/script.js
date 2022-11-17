eb473538c47385b58a3017eeee5f3bd7e1500966
zislamtanvir <zislamtanvir@gmail.com>
2022-11-17 10:58:24 +0600

    Show move made by player in gamebord
    Update bord based on specific symbol.
    
    When the game is between players,they have their own symbol that will use to
    play.And the move will be in the specific place of the board.This function
    will add symbol to the specific place of the board.If there any no symbol
    present.
    
    The playerMove function will add the symbol where the user click,if the
    box is already empty.and keep the symbol in gameboard array.By render the
    gameboard we will able to see the move in the ui.

--- script/script.js
+++ script/script.js
@@ -1,28 +1,52 @@
-const gameBord = function () {
-    let bord = [
-        ["o", "x", "o"],
-        ["x", "o", "x"],
-        ["o", "o", " x"],
+const gameboard = function () {
+    let board = [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""],
     ];
-    const getBord = () => {
-        return bord;
+    const getboard = () => {
+        return board;
     };
-    const updateBord = (row, col, value) => {
-        bord[row][col] = value;
+    const updateboard = (row, col, value) => {
+        board[row][col] = value;
     };
-    return { getBord, updateBord };
+    return { getboard, updateboard };
 };
-const renderBord = function (bord) {
-    let selectBordEl = document.querySelectorAll(".item");
-    let selectBordArr = [...selectBordEl];
-    let bordCounter = 0;
-    for (let i = 0; i < bord.length; i++) {
+const renderboard = function (board) {
+    let selectboardEl = document.querySelectorAll(".item");
+    let selectboardArr = [...selectboardEl];
+    let boardCounter = 0;
+    for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < 3; j++) {
-            selectBordArr[bordCounter].textContent = bord[i][j];
-            bordCounter++;
+            selectboardArr[boardCounter].textContent = board[i][j];
+            boardCounter++;
         }
     }
 };
 
-let mainBord = gameBord();
-renderBord(mainBord.getBord());
+const playerMove = (symbol) => {
+    let selectboardEl = document.querySelectorAll(".item");
+    let selectboardArr = [...selectboardEl];
+    const ticboard = gameboard();
+    selectboardArr.forEach((box) => {
+        box.addEventListener("click", function (e) {
+            console.log(e.target);
+            // Only able to add item it no move has made to that box
+            // means when the box is empty
+            if (e.target.textContent == "") {
+                let boxIndex = e.target.dataset.rowcol;
+                boxIndex = boxIndex.split("");
+                let row = Number(boxIndex[0]);
+                let col = Number(boxIndex[1]);
+                ticboard.updateboard(row, col, symbol);
+                console.log(ticboard.getboard());
+                renderboard(ticboard.getboard());
+            }
+        });
+    });
+};
+
+playerMove("-");
+
+let mainboard = gameboard();
+renderboard(mainboard.getboard());
