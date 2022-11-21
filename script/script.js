a98dcb372b119a660e82fa7f497b4f6b915ceab4
zahirul <zahirulopel@gmail.com>
2022-11-21 20:48:26 +0600

    Game winning conditon changed for cross row and column

--- script/script.js
+++ script/script.js
@@ -65,53 +65,102 @@ playerMove(player2.playerSymbol);
 
 // Check row and column are same
 let gameLogic = [
-  ["o", "o", "x"],
-  ["o", "x", "o"],
-  ["x", "o", "x"],
+  ["o", "x", "x"],
+  ["o", "x", "x"],
+  ["x", "o", "o"],
 ];
-// row check
-for (let row of gameLogic) {
-  if (!(row.includes("o") && row.includes("x")) && !row.includes("")) {
-    console.log(row);
-  }
-}
-// column check
-let i = 0;
-let j = 0;
-for (let round = 0; round < 3; round++) {
-  if (!gameLogic[round].includes("")) {
-    if (gameLogic[i][j] == gameLogic[i + 1][j]) {
-      if (gameLogic[i + 1][j] == gameLogic[i + 2][j]) {
-        console.log(gameLogic[i][j]);
-        break;
-      }
+const checkWinOrDraw = (gameBordArray) => {
+  //row check
+  for (let row of gameLogic) {
+    if (!(row.includes("o") && row.includes("x")) && !row.includes("")) {
+      console.log("row check:", row);
+      return row;
     }
   }
-  j++;
-}
-// cross check
-let k = 0;
-let l = 0;
-for (let round = 0; round < 2; round++) {
-  if (!gameLogic[round].includes("")) {
-    console.log("l value", l);
+  // column check
+  let i = 0;
+  let j = 0;
+  for (let round = 0; round < 3; round++) {
+    if (!gameLogic[round].includes("")) {
+      if (gameLogic[i][j] == gameLogic[i + 1][j]) {
+        if (gameLogic[i + 1][j] == gameLogic[i + 2][j]) {
+          console.log("column check:", gameLogic[i][j]);
+          return gameLogic[i][j];
+        }
+      }
+    }
+    j++;
+  }
+  // cross check
+  let k = 0;
+  let l = 0;
+  for (let round = 0; round < 2; round++) {
     // check 00,11,22 index
     if (l == 0) {
-      console.log("left kl", k, l);
       if (gameLogic[k][l] == gameLogic[k + 1][l + 1]) {
         if (gameLogic[k + 1][l + 1] == gameLogic[k + 2][l + 2]) {
-          console.log(gameLogic[k][l]);
+          console.log("left cross", gameLogic[k][l]);
+          return gameLogic[k][l];
         }
       }
     } // check 02,11,20 index
     else if (l == 2) {
-      console.log("right kl", k, l);
       if (gameLogic[k][l] == gameLogic[k + 1][l - 1]) {
         if (gameLogic[k + 1][l - 1] == gameLogic[k + 2][l - 2]) {
-          console.log(gameLogic[k][l]);
+          console.log("Right cross:", gameLogic[k][l]);
+          return gameLogic[k][l];
         }
       }
     }
+
+    l += 2;
   }
-  l += 2;
-}
+};
+let result = checkWinOrDraw(gameLogic);
+console.log(result);
+// // row check
+// for (let row of gameLogic) {
+//   if (!(row.includes("o") && row.includes("x")) && !row.includes("")) {
+//     console.log(row);
+//   }
+// }
+// // column check
+// let i = 0;
+// let j = 0;
+// for (let round = 0; round < 3; round++) {
+//   if (!gameLogic[round].includes("")) {
+//     if (gameLogic[i][j] == gameLogic[i + 1][j]) {
+//       if (gameLogic[i + 1][j] == gameLogic[i + 2][j]) {
+//         console.log(gameLogic[i][j]);
+//         break;
+//       }
+//     }
+//   }
+//   j++;
+// }
+// // cross check
+// let k = 0;
+// let l = 0;
+// for (let round = 0; round < 2; round++) {
+//   if (!gameLogic[round].includes("")) {
+//     console.log("l value", l);
+//     // check 00,11,22 index
+//     if (l == 0) {
+//       console.log("left kl", k, l);
+//       if (gameLogic[k][l] == gameLogic[k + 1][l + 1]) {
+//         if (gameLogic[k + 1][l + 1] == gameLogic[k + 2][l + 2]) {
+//           console.log(gameLogic[k][l]);
+//         }
+//       }
+//     } // check 02,11,20 index
+//     else if (l == 2) {
+//       console.log("right kl", k, l);
+//       if (gameLogic[k][l] == gameLogic[k + 1][l - 1]) {
+//         if (gameLogic[k + 1][l - 1] == gameLogic[k + 2][l - 2]) {
+//           console.log(gameLogic[k][l]);
+//         }
+//       }
+//     }
+//   }
+//   l += 2;
+// }
